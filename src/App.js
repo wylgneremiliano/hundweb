87255f76c008cc3c7c95e6fe87a75cdeffcebd9c
Wylgner Emiliano <wylgneremiliano@gmail.com>
2019-09-26 15:49:35 -0300

    detalhes de produto e configuração de rotas

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Header from "./components/Header";
 import api from "./services/api";
 import Main from "./pages/main";
+import Routes from './routes'
 import "./styles.css";
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Routes />
     </div>
   );
 }
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
 import "./styles.css";
 export default class Main extends Component {
   state = {
@@ -38,12 +39,21 @@ export default class Main extends Component {
           <article key={product._id}>
             <strong>{product.title}</strong>
             <p>{product.description}</p>
-            <a href=""> Acessar</a>
+            <Link to={`/products/${product._id}`}> Acessar</Link>
           </article>
         ))}
         <div className="actions">
-          <button disabled={page === 1} onClick={this.prevPage}> Anterior </button>
-          <button disabled={page === productsInfo.pages} onClick={this.nextPage}> Próximo </button>
+          <button disabled={page === 1} onClick={this.prevPage}>
+            {" "}
+            Anterior{" "}
+          </button>
+          <button
+            disabled={page === productsInfo.pages}
+            onClick={this.nextPage}
+          >
+            {" "}
+            Próximo{" "}
+          </button>
         </div>
       </div>
     );

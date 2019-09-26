c759d321667c071953b80d6653966cb9509410e8
Wylgner Emiliano <wylgneremiliano@gmail.com>
2019-09-26 15:08:52 -0300

    Paginação de produtos

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,28 +1,50 @@
 import React, { Component } from "react";
 import api from "../../services/api";
-import './styles.css'
+import "./styles.css";
 export default class Main extends Component {
   state = {
-    products: []
+    products: [],
+    productsInfo: {},
+    page: 1
   };
   componentDidMount() {
     this.loadProducts();
   }
-  loadProducts = async () => {
-    const response = await api.get("/products");
-    this.setState({ products: response.data.docs });
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
+    const { docs, ...productsInfo } = response.data;
+    this.setState({ products: docs, productsInfo, page });
+  };
+
+  prevPage = () => {
+    const { page, productsInfo } = this.state;
+    if (page == 1) return;
+    const pageNumber = page - 1;
+    this.loadProducts(pageNumber);
+  };
+  nextPage = () => {
+    const { page, productsInfo } = this.state;
+
+    if (page === productsInfo.pages) return;
+
+    const pageNumber = page + 1;
+    this.loadProducts(pageNumber);
   };
   render() {
-    const { products } = this.state;
+    const { products, page, productsInfo } = this.state;
     return (
       <div className="product-list">
         {products.map(product => (
           <article key={product._id}>
-              <strong>{product.title}</strong>
-              <p>{product.description}</p>
-              <a href=""> Acessar</a>
+            <strong>{product.title}</strong>
+            <p>{product.description}</p>
+            <a href=""> Acessar</a>
           </article>
         ))}
+        <div className="actions">
+          <button disabled={page === 1} onClick={this.prevPage}> Anterior </button>
+          <button disabled={page === productsInfo.pages} onClick={this.nextPage}> Próximo </button>
+        </div>
       </div>
     );
   }
